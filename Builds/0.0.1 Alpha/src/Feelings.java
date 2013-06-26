89d3b53ee4b4862928dc1ab83ed723c23194dbf9
ethansattler <mcdasethan@gmail.com>
2013-06-26 01:15:15 -0400

    Updated

---
+++ Builds/0.0.1 Alpha/src/Feelings.java
@@ -0,0 +1,8 @@
+class Feelings 
+{
+	public static void main (String[] args) 
+	{
+		System.out.println("Not the correct way to execute this file, refer to documentation.");
+		System.exit(0);
+	}
+}
