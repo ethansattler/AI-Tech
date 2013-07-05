e71f4c745137ba6e941e3c33996aec8b1e4f4eaa
ethansattler <mcdasethan@gmail.com>
2013-07-04 20:35:42 -0400

    Project Continued

--- Builds/.metadata/.plugins/org.eclipse.core.resources/.history/c0/4045a2381ede001211a9c37ed9cbf8ba
+++
@@ -1,8 +0,0 @@
-class feelings 
-{
-	public static void main (String[] args) 
-	{
-		System.out.println("Not the correct way to execute this file, refer to documentation.");
-		System.exit(0);
-	}
-}
--- Builds/0.0.1 Alpha/src/Feelings.java
+++
@@ -1,8 +0,0 @@
-class Feelings 
-{
-	public static void main (String[] args) 
-	{
-		System.out.println("Not the correct way to execute this file, refer to documentation.");
-		System.exit(0);
-	}
-}
